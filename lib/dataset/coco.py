c88b78336f072876897bbb60249593b7cafafb74
unknown <ci.gonzalez10@uniandes.edu.co>
2018-11-02 23:39:01 -0500

    Debug

--- lib/dataset/coco.py
+++ lib/dataset/coco.py
@@ -390,6 +390,7 @@ class COCODataset(JointsDataset):
         return cat_results
 
     def _do_python_keypoint_eval(self, res_file, res_folder):
+        pdb.set_trace()
         coco_dt = self.coco.loadRes(res_file)
         coco_eval = COCOeval(self.coco, coco_dt, 'keypoints')
         coco_eval.params.useSegm = None
