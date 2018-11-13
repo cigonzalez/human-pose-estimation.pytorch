72a585771ffb6b1832081cb1120b4853e42fa10e
unknown <ci.gonzalez10@uniandes.edu.co>
2018-11-13 13:19:34 -0500

    Debug

--- lib/core/function.py
+++ lib/core/function.py
@@ -180,7 +180,7 @@ def validate(config, val_loader, val_dataset, model, criterion, output_dir,
                 save_debug_images(config, input, meta, target, pred*4, output,
                                   prefix)
 
-        #pdb.set_trace()
+        pdb.set_trace()
         name_values, perf_indicator = val_dataset.evaluate(
             config, all_preds, output_dir, all_boxes, image_path,
             filenames, imgnums)
